6c66cdb28c734c6a1b6b2cafabdc14fe2b12d418
JeanCruz2025 <165338134+JeanCruz2025@users.noreply.github.com>
2024-04-05 21:01:52 -0300

    Add files via upload

---
+++ exercicio 3 sqrt.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+int raiz;
+
+{
+    printf("RAIZ QUADRADA\n");
+    printf("Digite um valor positivo:\n");
+    scanf("%i" , &raiz );
+}
+
+printf("%f", sqrt(raiz));
+
+    return 0;
+}
+
+
